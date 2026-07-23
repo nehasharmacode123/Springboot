a8d91973f486dbe6026e5d590184f67366b3b92b
nehasharmacode123 <nehasharma962595@gmail.com>
2026-07-23 18:02:23 +0530

    learn class

---
+++ src/classAndObject/Car.java
@@ -0,0 +1,16 @@
+package classAndObject;
+
+public class Car {
+
+    String brand;
+    String color;
+    int speed;
+
+    void start() {
+        System.out.println("Car Started!");
+    }
+
+    void stop() {
+        System.out.println("Car Stopped!");
+    }
+}
--- src/classAndObject/LearnClass.java
+++ src/classAndObject/LearnClass.java
@@ -1,18 +1,39 @@
+//package classAndObject;
+//
+//public class LearnClass {
+//
+//    public static void main(String[] args) {
+//
+//        Car car1 = new Car();
+//
+//        car1.brand = "BMW";
+//        car1.color = "Black";
+//        car1.speed = 220;
+//
+//        System.out.println(car1.brand);
+//        System.out.println(car1.color);
+//
+//        car1.start();
+//        car1.stop();
+//    }
+//}
+
 package classAndObject;
 
 public class LearnClass {
-    Class Car {
 
-        String brand;
-        String color;
-        int speed;
+    public static void main(String[] args) {
 
-        void start() {
-            System.out.println("Car Started");
-        }
+        Car car1 = new Car();
 
-        void stop() {
-            System.out.println("Car Stopped");
-        }
+        car1.brand = "BMW";
+        car1.color = "Black";
+        car1.speed = 240;
+
+        System.out.println(car1.brand);
+        System.out.println(car1.color);
+
+        car1.start();
+        car1.stop();
     }
 }
